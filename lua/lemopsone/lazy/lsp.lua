070e11df0a5c32ce0aed3ca80631b454406f1e4a
lemopsone <maxim@varchenko.ru>
2024-10-02 19:03:46 +0300

    feat(lsp): lspsaga для TS, intelephense -> phpactor, Lua 5.1 -> 5.4, prettier для формата note: теперь нужно ставить prettier локально чтобы форматирование было хайповым

--- lua/lemopsone/lazy/lsp.lua
+++ lua/lemopsone/lazy/lsp.lua
@@ -11,16 +11,20 @@ return {
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
+		"onsails/lspkind-nvim",
+		"stevearc/conform.nvim",
 	},
 
 	config = function()
-		local cmp = require('cmp')
+		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
+		local luasnip = require("luasnip")
 		local capabilities = vim.tbl_deep_extend(
 			"force",
 			{},
 			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities())
+			cmp_lsp.default_capabilities()
+		)
 
 		require("fidget").setup({})
 		require("mason").setup()
@@ -29,52 +33,40 @@ return {
 				"lua_ls",
 				"rust_analyzer",
 				"gopls",
-				"intelephense",
-				"clangd"
+				"phpactor",
+				"clangd",
+				"tailwindcss",
+				"tsserver",
+				"pyright",
 			},
 			handlers = {
-				function(server_name)         -- default handler (optional)
-					require("lspconfig")[server_name].setup {
-						capabilities = capabilities
-					}
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
 				end,
 
 				["lua_ls"] = function()
 					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup {
+					lspconfig.lua_ls.setup({
 						capabilities = capabilities,
 						settings = {
 							Lua = {
-								runtime = { version = "Lua 5.1" },
+								runtime = { version = "Lua 5.4" },
 								diagnostics = {
 									globals = { "vim", "it", "describe", "before_each", "after_each" },
-								}
-							}
-						}
-					}
-				end,
-				["intelephense"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.intelephense.setup {
-						settings = {
-							intelephense = {
-								environment = {
-									includePaths = {
-										"~/work/portal2"
-									},
-									phpVersion = "8.2",
-								}
-							}
-						}
-					}
+								},
+							},
+						},
+					})
 				end,
 				["clangd"] = function()
 					local lspconfig = require("lspconfig")
-					lspconfig.clangd.setup {
-						filetypes = { "c", "cpp", "objc", "objcpp", "inc", "pov" }
-					}
-				end
-			}
+					lspconfig.clangd.setup({
+						filetypes = { "c", "cpp", "objc", "objcpp", "inc", "pov" },
+					})
+				end,
+			},
 		})
 
 		local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -82,21 +74,49 @@ return {
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					require('luasnip').lsp_expand(args.body)           -- For `luasnip` users.
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-				['<CR>'] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
+				["<CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if luasnip.expandable() then
+							luasnip.expand()
+						else
+							cmp.confirm({ select = true })
+						end
+					else
+						fallback()
+					end
+				end),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.locally_jumpable(1) then
+						luasnip.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'luasnip' },         -- For luasnip users.
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
 			}, {
-				{ name = 'buffer' },
-			})
+				{ name = "buffer" },
+			}),
+			formatting = {
+				format = require("lspkind").cmp_format({ with_text = true, maxwidth = 50 }),
+			},
 		})
 
 		vim.diagnostic.config({
@@ -117,6 +137,45 @@ return {
 				end,
 			})
 		end
-		vim.keymap.set("n", "<F3>", format_buffer, { buffer = bufnr })
-	end
+		--    vim.keymap.set("n", "<F3>", format_buffer, { buffer = bufnr })
+		-- Autoformatting setup
+		require("conform").setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+		})
+		--	vim.api.nvim_create_autocmd("BufWritePre", {
+		--		callback = function(args)
+		--			require("conform").format({
+		--				bufnr = args.bufnr,
+		--				lsp_fallback = true,
+		--				quiet = true,
+		--			})
+		--		end,
+		--	})
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_format = "fallback", range = range })
+		end, { range = true })
+		vim.keymap.set("n", "<F3>", ":Format<CR>")
+	end,
 }
